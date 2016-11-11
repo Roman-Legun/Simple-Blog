7ae61da78ca3c09d29827c658f5330d0e226f750
Роман Легун <legun.roman@yandex.ua>
2016-11-11 19:38:24 +0200

    Add show in Article

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,9 +1,24 @@
-class ArticlesController < ApplicationController
-	def new
-		
+class ArticlesController < ApplicationController 
+
+	def show
+		@article = Article.find(params[:id])
+	end
+
+	def new	
 	end
 
 	def create
-		render plain: params[:article].inspect
+		@article = Article.new(article_params)
+		if @article.valid?
+			@article.save
+			redirect_to @article
+		else
+			render action: 'new'
+		end
 	end
+
+	def article_params
+		params.require(:article).permit(:title, :text)
+	end
+
 end
